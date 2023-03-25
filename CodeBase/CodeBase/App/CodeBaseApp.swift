52b1c7655afa12d0d9ef50529b3dfba5dfe0cf50
Vladyslav Shkodych <vladyslav.shkodych@gmail.com>
2023-03-25 02:57:55 +0200

    [add] xcode project

---
+++ CodeBase/CodeBase/App/CodeBaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodeBaseApp.swift
+//  CodeBase
+//
+//  Created by Vladyslav Shkodych on 25.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct CodeBaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
